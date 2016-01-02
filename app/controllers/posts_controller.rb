ef6bbd4272ce7133c0d9ad0a69806b583c5df4a1
JosephWinfield <krazyjoe18@gmail.com>
2016-01-02 17:29:44 -0500

    blog form, post list, and posts pages

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  before_filter :authenticate, :except => [ :index, :show ]
+
+  def index
+    @posts = Post.order("id DESC").all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path, alert: 'Blog Post successfully deleted'
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Blog Post successfully added"
+      redirect_to post_path(@post)
+    else
+      flash[:notice] = "Operator Error!!"
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :body)
+  end
+
+  def authenticate
+  authenticate_or_request_with_http_basic do |name, password|
+    name == "admin" && password == "secret"
+    end
+  end
+
+end
